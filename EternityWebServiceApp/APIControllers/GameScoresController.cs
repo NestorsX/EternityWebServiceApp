3d2e3d0a839aea57c74796c74fabb23b83214134
Кирилл Нестерович <nesterkalol@gmail.com>
2022-06-06 02:32:58 +0300

    added modifications to GameScore api model for more convinience

--- EternityWebServiceApp/APIControllers/GameScoresController.cs
+++ EternityWebServiceApp/APIControllers/GameScoresController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using EternityWebServiceApp.Models;
 using EternityWebServiceApp.ViewModels;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Linq;
 
 namespace EternityWebServiceApp.APIControllers
 {
@@ -12,10 +16,12 @@ namespace EternityWebServiceApp.APIControllers
     public class GameScoresController : ControllerBase
     {
         private readonly EternityDBContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
 
-        public GameScoresController(EternityDBContext context)
+        public GameScoresController(EternityDBContext context, IWebHostEnvironment appEnvironment)
         {
             _context = context;
+            _appEnvironment = appEnvironment;
         }
 
         // Получает лучший результат по игре
@@ -35,10 +41,19 @@ namespace EternityWebServiceApp.APIControllers
             }
 
             gameScore.User = await _context.Users.FirstOrDefaultAsync(x => x.UserId == gameScore.UserId);
+            string image = null;
+            var path = $"{_appEnvironment.WebRootPath}/Images/Users/{gameScore.UserId}";
+            if (Directory.Exists(path))
+            {
+                image = $"{Directory.GetFiles(path).Select(x => Path.GetFileName(x)).First()}";
+            }
+
             var result = new GameScoreViewModel
             {
                 GameName = gameScore.Game.Name,
+                UserId = gameScore.UserId,
                 UserName = gameScore.User.UserName,
+                Image = image,
                 Score = gameScore.Score,
             };
 
@@ -65,14 +80,28 @@ namespace EternityWebServiceApp.APIControllers
                 return Ok();
             }
 
-            string[] bestTime = gameScore.Score.Split(':');
-            string[] newTime = newScore.Score.Split(':');
-            int.TryParse(bestTime[0], out int bestT);
-            int.TryParse(newTime[0], out int newT);
-            if (bestT == newT)
+            if (newScore.GameId == 1)
             {
-                int.TryParse(bestTime[1], out bestT);
-                int.TryParse(newTime[1], out newT);
+                string[] bestTime = gameScore.Score.Split(':');
+                string[] newTime = newScore.Score.Split(':');
+                int.TryParse(bestTime[0], out int bestT);
+                int.TryParse(newTime[0], out int newT);
+                if (bestT == newT)
+                {
+                    int.TryParse(bestTime[1], out bestT);
+                    int.TryParse(newTime[1], out newT);
+                    if (bestT > newT)
+                    {
+                        gameScore.Score = newScore.Score;
+                        gameScore.UserId = newScore.UserId;
+                        _context.GameScores.Update(gameScore);
+                        await _context.SaveChangesAsync();
+                        return Ok();
+                    }
+
+                    return Ok();
+                }
+
                 if (bestT > newT)
                 {
                     gameScore.Score = newScore.Score;
@@ -81,17 +110,23 @@ namespace EternityWebServiceApp.APIControllers
                     await _context.SaveChangesAsync();
                     return Ok();
                 }
-
-                return Ok();
             }
 
-            if (bestT > newT)
+            if (newScore.GameId == 2)
             {
-                gameScore.Score = newScore.Score;
-                gameScore.UserId = newScore.UserId;
-                _context.GameScores.Update(gameScore);
-                await _context.SaveChangesAsync();
-                return Ok();
+                if (gameScore.Score == newScore.Score)
+                {
+                    return Ok();
+                }
+
+                if (Convert.ToInt32(gameScore.Score) < Convert.ToInt32(newScore.Score))
+                {
+                    gameScore.Score = newScore.Score;
+                    gameScore.UserId = newScore.UserId;
+                    _context.GameScores.Update(gameScore);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
             }
 
             return Ok();
--- EternityWebServiceApp/ViewModels/GameScoreViewModel.cs
+++ EternityWebServiceApp/ViewModels/GameScoreViewModel.cs
@@ -3,7 +3,9 @@
     public class GameScoreViewModel
     {
         public string GameName { get; set; }
+        public int UserId { get; set; }
         public string UserName { get; set; }
+        public string Image { get; set; }
         public string Score { get; set; }
     }
 }
